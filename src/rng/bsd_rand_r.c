c750f8689ac0085bb3ca1e9cfdca03251caffc40
Lynn Kirby <lynn@arrak.is>
2020-01-05 06:52:48 -0500

    Rename openbsd_rand_r to bsd_rand_r
    This is really the original implementation of rand_r() (probably originating
    from NetBSD) and doesn't contain code specific to OpenBSD.

--- src/rng/bsd_rand_r.c
+++ src/rng/bsd_rand_r.c
@@ -8,12 +8,12 @@
 
 unsigned state;
 
-void openbsd_rand_r_init(uint32_t seed)
+void bsd_rand_r_init(uint32_t seed)
 {
     state = seed;
 }
 
-uint32_t openbsd_rand_r_next()
+uint32_t bsd_rand_r_next()
 {
 	state = state * 1103515245 + 12345;
 	return (state & RAND_MAX);

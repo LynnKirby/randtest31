1de4975a44d8ce211a853236a4fba57461e6e1b4
Lynn Kirby <lynn@arrak.is>
2020-01-05 09:34:01 -0500

    Implement pcg32 RNG
    Adds PCG XSH-RR 64/32 (LCG), also known as pcg32.

---
+++ src/rng/pcg32.c
@@ -0,0 +1,20 @@
+/*
+ * SPDX-License-Identifier: CC0-1.0
+ * SPDX-FileCopyrightText: 2020 Lynn Kirby
+ */
+
+#include <stdint.h>
+#include <limits.h>
+#include "pcg_variants.h"
+
+static pcg32s_random_t state;
+
+void pcg32_init(uint32_t seed)
+{
+    pcg32s_srandom_r(&state, seed);
+}
+
+uint32_t pcg32_next()
+{
+    return pcg32s_random_r(&state);
+}
